8386fbdb4d931399ed84161c649a094b834f5ebf
DrewBoschert <drew.r.boschert@gmail.com>
2023-09-26 15:58:27 -0400

    Added Login

---
+++ BlackjackConsoleApp/Models/PlayerController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace BlackjackConsoleApp.Models
+{
+    public class PlayerController
+    {
+        const string baseurl = "http://localhost:8080";
+        // PLAYER LOGIN
+        public static async Task<JsonResponse> PlayerLoginAsync(HttpClient http, JsonSerializerOptions joptions, string username, string password)
+        {
+            HttpRequestMessage req = new HttpRequestMessage(HttpMethod.Get, $"{baseurl}/api/players/{username}/{password}");
+            HttpResponseMessage res = await http.SendAsync(req);
+            if (res.StatusCode != System.Net.HttpStatusCode.OK)
+            {
+                Console.WriteLine($"Http ErrorCode: {res.StatusCode}");
+            }
+            var json = await res.Content.ReadAsStringAsync();
+            var player = (Player?)JsonSerializer.Deserialize(json, typeof(Player), joptions);
+            if (player is null) throw new Exception();
+            return new JsonResponse
+            {
+                HttpStatusCode = (int)res.StatusCode,
+                DataReturned = player
+            };
+        }
+
+
+
+    }
+}
