50046a17504eb512b7c3bae5622ac55f899015f6
Josh Stothfang <josh@joshstothfang.com>
2023-09-27 09:59:03 -0400

    Added MainMenu method

--- BlackjackConsoleApp/Program.cs
+++ BlackjackConsoleApp/Program.cs
@@ -1,48 +1,20 @@
 using BlackjackConsoleApp;
 
+Player? player = await Blackjack.LoginPrompt();
 
 
-
-
-await Blackjack.LoginPrompt();
-
-
-
-// The problem atm is that when you put in a username and password that isn't
-// in the database the code runs into an error because it only checks if the player is nu
-// it needs to check if the player is in the database
-
-
-/*
-async Task<Player> LoginPrompt()
+switch(Blackjack.MainMenu())
 {
-    while (true)
-    {
-        Console.Write("Enter Username: ");
-        string? username = Console.ReadLine();
-        Console.Write("Enter Password: ");
-        string? password = Console.ReadLine();
-        jsonResponse = await PlayerController.PlayerLoginAsync(username!, password!);
-        player = jsonResponse.DataReturned as Player;
-        var status = jsonResponse.HttpStatusCode;
-        Console.WriteLine($"status = {status}");
-        if (player == null || status == 404)
-        {
-            Console.WriteLine("\n[Login Failed!]");
-            Console.Write("[PRESS ENTER]");
-            Console.ReadLine();
-            Console.Clear();
-            continue;
-        }
-
-        Console.WriteLine("GREAT");
-        Console.WriteLine(player.LastName);
+    case "1":
+        //play game
         break;
-    }
-    return player;
+    case "2":
+        //deposit
+        break;
+    case "3":
+        //exit
+        break;
 }
-*/
-
 
 /*
 var p1 = new Player()
